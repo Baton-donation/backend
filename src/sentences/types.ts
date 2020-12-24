6654cddda0eabf691c16d06dcbdae66be5c22f30
Max Isom <hi@maxisom.me>
2020-12-24 13:13:45 -0600

    Add initial tests

--- src/sentences/types.ts
+++ src/sentences/types.ts
@@ -2,13 +2,13 @@ import { IsNotEmpty, IsUUID } from "class-validator";
 
 export class SentenceDto {
   @IsUUID("4")
-  uuid: string;
+  uuid!: string;
 
   @IsNotEmpty()
-  content: string;
+  content!: string;
 }
 
 export class DeleteParams {
   @IsUUID("4")
-  uuid: string;
+  uuid!: string;
 }
---
+++ test/sentences.e2e-spec.ts
@@ -0,0 +1,59 @@
+
+import * as request from 'supertest';
+import {v4 as uuidv4} from 'uuid'
+import { Test } from '@nestjs/testing';
+import {AppModule} from 'src/app.module'
+// import {SentencesService} from '../src/sentences/sentences.service';
+import { INestApplication } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+
+describe('Sentences', () => {
+  let app: INestApplication;
+  let prismaService = {
+    sentence: {
+      create: (payload: any) => Promise.resolve(payload.data),
+      delete: () => Promise.resolve()
+    }
+  };
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(prismaService)
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it('/POST sentences', async () => {
+    const payload = {uuid: uuidv4(), content: 'A sample sentence'};
+
+    const spy = jest.spyOn(prismaService.sentence, 'create');
+
+    await request(app.getHttpServer())
+      .post('/sentences')
+      .send(payload)
+      .expect(201, payload)
+
+    expect(spy).toHaveBeenCalledWith({data: payload});
+  });
+
+  it('/DELETE sentences', async () => {
+    const uuid = uuidv4();
+
+    const spy = jest.spyOn(prismaService.sentence, 'delete');
+
+    await request(app.getHttpServer())
+      .delete(`/sentences/${uuid}`)
+      .expect(200);
+
+    expect(spy).toHaveBeenCalledWith({where: {uuid}});
+  })
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
